a23b5e8d06b7e4d9c5d8b3bb64fd867718b1b1ac
IstiSama <137089345+IstiSama@users.noreply.github.com>
2024-11-11 17:35:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Wéber István");
+
+let city = "Kapuvár";
+let country = " Magyarország";
+
+console.log(city + country);
+
+let x = 9;
+
+console.log(x);
+
+console.log(x + 1);
+
+let y = 20;
+
+console.log(x + y);
+
+console.log(x * y);
+
+let z = 2641;
+
+console.log(2641 / 7);
+
+let favouritePet = prompt("Mi a kedvenc állatod?");
+console.log("A kedvenc állatod a t" + favouritePet);
